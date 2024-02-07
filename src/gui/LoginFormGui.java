cc3beb2cd158201a2d29ef4a756e2a979a0a14a6
Solanki Singha <solankisingha23@gmail.com>
2024-02-07 20:37:24 +0530

    Font style and size is modified. User input validation is added

--- src/gui/LoginFormGui.java
+++ src/gui/LoginFormGui.java
@@ -17,107 +17,108 @@ import javax.swing.SwingConstants;
 import Constant.CommonConstants;
 import backend.MyJDBC;
 
-public class LoginFormGui extends Form{
-    public LoginFormGui(){
+public class LoginFormGui extends Form {
+    public LoginFormGui() {
         super("Login");
         addGUIComponents();
     }
-    public void addGUIComponents(){
-        //Create a Login Label
+
+    public void addGUIComponents() {
+        // Create a Login Label
         JLabel loginLabel = new JLabel("Login");
-        //configure components x,y position and width/height values relatively
+        // configure components x,y position and width/height values relatively
         loginLabel.setBounds(0, 25, 520, 100);
-        //change the font color
+        // change the font color
         loginLabel.setForeground(CommonConstants.TEXT_COLOR);
-        //change the font size
-        loginLabel.setFont(new Font("Dialog", Font.BOLD, 40));
-        //center text
+        // change the font size
+        loginLabel.setFont(new Font("Bell MT", Font.BOLD, 40));
+        // center text
         loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        //Create a user name label
+        // Create a user name label
         JLabel usernameLabel = new JLabel("Username:");
-        usernameLabel.setBounds(30,150,400,25);
+        usernameLabel.setBounds(30, 150, 400, 25);
         usernameLabel.setForeground(CommonConstants.TEXT_COLOR);
-        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
+        usernameLabel.setFont(new Font("Bell MT", Font.BOLD, 24));
 
-        //Create a username text field
+        // Create a username text field
         JTextField userJTextField = new JTextField();
-        userJTextField.setBounds(30,185,450,55);
+        userJTextField.setBounds(30, 185, 450, 55);
         userJTextField.setBackground(CommonConstants.SECONDARY_COLOR);
         userJTextField.setForeground(CommonConstants.TEXT_COLOR);
-        userJTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        userJTextField.setFont(new Font("Dialog", Font.PLAIN, 22));
 
-        //create a password label
+        // create a password label
         JLabel pwLabel = new JLabel("Password:");
-        pwLabel.setBounds(30,335,400,25);
+        pwLabel.setBounds(30, 335, 400, 25);
         pwLabel.setForeground(CommonConstants.TEXT_COLOR);
-        pwLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
+        pwLabel.setFont(new Font("Bell MT", Font.BOLD, 24));
 
-        //Create a password text field
+        // Create a password text field
         JPasswordField pwTextField = new JPasswordField();
-        pwTextField.setBounds(30,365,450,55);
+        pwTextField.setBounds(30, 365, 450, 55);
         pwTextField.setBackground(CommonConstants.SECONDARY_COLOR);
         pwTextField.setForeground(CommonConstants.TEXT_COLOR);
-        pwTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        pwTextField.setFont(new Font("Dialog", Font.PLAIN, 22));
 
-        //create a Login button
+        // create a Login button
         JButton loginButton = new JButton("Login");
-        loginButton.setBounds(125,520,250,50);
-        loginButton.setFont(new Font("Dialog", Font.BOLD, 18));
+        loginButton.setBounds(125, 520, 250, 50);
+        loginButton.setFont(new Font("Bell MT", Font.BOLD, 22));
         loginButton.setBackground(CommonConstants.SECONDARY_COLOR);
+        loginButton.setForeground(CommonConstants.TEXT_COLOR);
         loginButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        //------ Add login credential Validation ------
         loginButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                //we will first retrive the username and password value from our user
-                //get username
+                // we will first retrive the username and password value from our user
+                // get username & password
                 String username = userJTextField.getText();
-
-                //get password
                 String password = new String(pwTextField.getPassword());
 
-                // chech database if the username and password combo is valid or not 
-                if(MyJDBC.validateLogin(username, password)){
+                // chech database if the username and password combo is valid or not
+                if (MyJDBC.validateLogin(username, password)) {
                     // login successful
-                    JOptionPane.showMessageDialog(LoginFormGui.this, "Login Successful...");
+                    JOptionPane.showMessageDialog(LoginFormGui.this, "Login Successful!");
 
                     // close the current window and open WelcomePage
                     setVisible(false);
                     new WelcomePage(username).setVisible(true);
-                    
-                }else{
-                    //login failed
-                    JOptionPane.showMessageDialog(LoginFormGui.this, "Login failed...");
+
+                } else {
+                    // login failed
+                    JOptionPane.showMessageDialog(LoginFormGui.this, "Login failed...!\n You are redirect to the Registration Form.");
+                    // dispose or close Login GUI and open Register GUI
+                    LoginFormGui.this.dispose();
+                    new RegisterFormGUI().setVisible(true);
                 }
             }
-            
+
         });
 
-        //create a Register label to load the register GUI
+        // create a Register label to load the register GUI
         JLabel registerLabel = new JLabel("Not a User? Register here");
         registerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        registerLabel.setBounds(125,600,250,30);
+        registerLabel.setBounds(125, 600, 250, 30);
         registerLabel.setForeground(CommonConstants.TEXT_COLOR);
         registerLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        //add Functionality so that when clicked it will launch the Register Form GUI
+        // add Functionality so that when clicked it will launch the Register Form GUI
         registerLabel.addMouseListener(new MouseAdapter() {
 
             @Override
             public void mouseClicked(MouseEvent e) {
-                //dispose or close Login GUI
+                // dispose or close Login GUI
                 LoginFormGui.this.dispose();
-                //launch the register GUI
+                // launch the register GUI
                 new RegisterFormGUI().setVisible(true);
             }
-            
+
         });
 
-
-        //add components
+        // add components
         add(loginLabel);
         add(usernameLabel);
         add(userJTextField);
--- src/gui/RegisterFormGUI.java
+++ src/gui/RegisterFormGUI.java
@@ -18,130 +18,116 @@ import javax.swing.SwingConstants;
 import Constant.CommonConstants;
 import backend.MyJDBC;
 
-public class RegisterFormGUI extends Form{
-    public RegisterFormGUI(){
-        super("Login");
+public class RegisterFormGUI extends Form {
+    public RegisterFormGUI() {
+        super("Register");
         addGUIComponents();
     }
 
-    public void addGUIComponents(){
-        //Create a register Label
+    public void addGUIComponents() {
+
+        // Create a register Label
         JLabel registerLabel = new JLabel("Register");
-        //configure components x,y position and width/height values relatively
         registerLabel.setBounds(0, 25, 520, 100);
-        //change the font color
         registerLabel.setForeground(CommonConstants.TEXT_COLOR);
-        //change the font size
-        registerLabel.setFont(new Font("Dialog", Font.BOLD, 40));
-        //center text
+        registerLabel.setFont(new Font("Bell MT", Font.BOLD, 40));
         registerLabel.setHorizontalAlignment(SwingConstants.CENTER);
 
-        //Create a user name label
+        // Create a user name label
         JLabel usernameLabel = new JLabel("Username:");
-        usernameLabel.setBounds(30,150,400,25);
+        usernameLabel.setBounds(30, 150, 400, 25);
         usernameLabel.setForeground(CommonConstants.TEXT_COLOR);
-        usernameLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
+        usernameLabel.setFont(new Font("Bell MT", Font.BOLD, 24));
 
-        //Create a username text field
+        // Create a username text field
         JTextField userJTextField = new JTextField();
-        userJTextField.setBounds(30,185,450,55);
+        userJTextField.setBounds(30, 185, 450, 55);
         userJTextField.setBackground(CommonConstants.SECONDARY_COLOR);
         userJTextField.setForeground(CommonConstants.TEXT_COLOR);
-        userJTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        userJTextField.setFont(new Font("Dialog", Font.PLAIN, 22));
 
-        //create a password label
+        // create a password label
         JLabel pwLabel = new JLabel("Password:");
-        pwLabel.setBounds(30,255,400,25);
+        pwLabel.setBounds(30, 260, 400, 25);
         pwLabel.setForeground(CommonConstants.TEXT_COLOR);
-        pwLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
+        pwLabel.setFont(new Font("Bell MT", Font.BOLD, 24));
 
-        //Create a password text field for password label
+        // Create a password text field for password label
         JPasswordField pwTextField = new JPasswordField();
-        pwTextField.setBounds(30,300,450,55);
+        pwTextField.setBounds(30, 290, 450, 55);
         pwTextField.setBackground(CommonConstants.SECONDARY_COLOR);
         pwTextField.setForeground(CommonConstants.TEXT_COLOR);
-        pwTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        pwTextField.setFont(new Font("Dialog", Font.PLAIN, 22));
 
-        //Re-enter Password Label
+        // Re-enter Password Label
         JLabel reEnterpwLabel = new JLabel("Re-Enter Your Password:");
-        reEnterpwLabel.setBounds(30,365,400,25);
+        reEnterpwLabel.setBounds(30, 365, 400, 25);
         reEnterpwLabel.setForeground(CommonConstants.TEXT_COLOR);
-        reEnterpwLabel.setFont(new Font("Dialog", Font.PLAIN, 18));
+        reEnterpwLabel.setFont(new Font("Bell MT", Font.BOLD, 24));
 
         JPasswordField reEnterpwTextField = new JPasswordField();
-        reEnterpwTextField.setBounds(30,395,450,55);
+        reEnterpwTextField.setBounds(30, 395, 450, 55);
         reEnterpwTextField.setBackground(CommonConstants.SECONDARY_COLOR);
         reEnterpwTextField.setForeground(CommonConstants.TEXT_COLOR);
-        reEnterpwTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
+        reEnterpwTextField.setFont(new Font("Dialog", Font.PLAIN, 22));
 
-
-        //create a Register button
+        // create a Register button
         JButton registerButton = new JButton("Register");
-        registerButton.setBounds(125,520,250,50);
-        registerButton.setFont(new Font("Dialog", Font.BOLD, 18));
+        registerButton.setBounds(125, 520, 250, 50);
+        registerButton.setFont(new Font("Bell MT", Font.BOLD, 22));
         registerButton.setBackground(CommonConstants.SECONDARY_COLOR);
+        registerButton.setForeground(CommonConstants.TEXT_COLOR);
         registerButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        // -------- Backend code goes here ---------
         registerButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                //get username
+                // get username, password and Re-entered password
                 String username = userJTextField.getText();
-
-                //get password
                 String password = new String(pwTextField.getPassword());
-
-                //get Re-entered password
-                String rePassword = new String(pwTextField.getPassword());
+                String rePassword = new String(reEnterpwTextField.getPassword());
 
                 // validate user input
-                if(validateUserInput(username, password, rePassword)){
+                if (validateUserInput(username, password, rePassword, pwTextField, reEnterpwTextField)) {
                     // register the user to the database
-                    if(MyJDBC.register(username, rePassword)){
-                        // dispose of this GUI
-                        RegisterFormGUI.this.dispose();
+                    if (MyJDBC.register(username, rePassword)) {
 
-                        //take user back to the login GUI
+                        // dispose of this GUI and open Login Form GUI
+                        RegisterFormGUI.this.dispose();
                         LoginFormGui loginFormGui = new LoginFormGui();
                         loginFormGui.setVisible(true);
 
-                        //create a result dialog box
-                        JOptionPane.showMessageDialog(loginFormGui, "Registered account successfully!...");
-                    }else{
-                        //register failed (likely due to the user is already exists in the database)
+                        JOptionPane.showMessageDialog(loginFormGui, "Account registered successfully!");
+                    } else {
+                        // register failed (likely due to the user is already exists in the database)
                         JOptionPane.showMessageDialog(RegisterFormGUI.this, "Error: Username is already taken!");
                     }
                 }
-                else{
-                    //invalid user input
-                    JOptionPane.showMessageDialog(RegisterFormGUI.this, "Error: Username must be atleast 6 character long \n or password must match");
-                }
             }
-            
+
         });
 
-        //create a Login label to load the Login GUI
+        // create a Login label to load the Login GUI
         JLabel loginJLabel = new JLabel("Have an account? Login here");
         loginJLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        loginJLabel.setBounds(125,600,250,30);
+        loginJLabel.setBounds(125, 600, 250, 30);
         loginJLabel.setForeground(CommonConstants.TEXT_COLOR);
         loginJLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-         //add Functionality so that when clicked it will launch the Login Form GUI
-         loginJLabel.addMouseListener((MouseListener) new MouseAdapter() {
+        // add Functionality so that when clicked it will launch the Login Form GUI
+        loginJLabel.addMouseListener((MouseListener) new MouseAdapter() {
 
             @Override
             public void mouseClicked(MouseEvent e) {
-                //dispose or close Login GUI
+                // dispose or close Login GUI
                 RegisterFormGUI.this.dispose();
-                //launch the register GUI
+                // launch the register GUI
                 new LoginFormGui().setVisible(true);
             }
         });
 
-        //add components
+        // add components
         add(registerLabel);
         add(usernameLabel);
         add(userJTextField);
@@ -151,23 +137,27 @@ public class RegisterFormGUI extends Form{
         add(reEnterpwTextField);
         add(registerButton);
         add(loginJLabel);
-        
+
     }
-    
-    //so here we are going to validate our user input, making sure that user has placed a valid username and password
-    private boolean validateUserInput(String username, String password, String rePassword){
-        //all fields must have a value
-        if(username.length() == 0 || password.length() == 0 || rePassword.length() == 0){
-            return false;
-        }
 
-        //username has to be atleast 6 characters long
-        if(username.length() < 6){
-            return false;
-        }
+    // so here we are going to validate our user input, making sure that user has
+    // placed a valid username and password
+    private boolean validateUserInput(String username, String password, String rePassword, JPasswordField pwTextField, JPasswordField reEnterpwTextField) {
 
-        //password and rePassword must be same like username
-        if(!password.equals(rePassword)){
+        if(username.isBlank() || password.isBlank() || rePassword.isBlank()){
+            JOptionPane.showMessageDialog(null, "Please fill all the above details");
+            return false;
+
+        } else if (username.length()< 6 || password.length()<6 || rePassword.length()<6){
+            JOptionPane.showMessageDialog(null, "Username and Password should be 6 characters long!");
+            pwTextField.setText("");
+            reEnterpwTextField.setText("");
+            return false;
+
+        } else if (!password.equals(rePassword)){
+            JOptionPane.showMessageDialog(null, "Entered password mismatched! \nRe-enter you password!");
+            pwTextField.setText("");
+            reEnterpwTextField.setText("");
             return false;
         }
 
